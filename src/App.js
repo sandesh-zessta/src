5324fa8dcab90bc12fc70c5ccf6cb28aaf5ae39f
Anup-Zessta <anup.bal@zessta.com>
2023-03-17 16:42:29 +0530

    Day 2 React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
+  const expense = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 230,
+      Date: new Date(2023, 3, 18),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 930,
+      Date: new Date(2023, 3, 25),
+    },
+    {
+      id: "e3",
+      title: "New TV",
+      amount: 550,
+      Date: new Date(2023, 3, 30),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expenses items={expense}></Expenses>
     </div>
   );
 }
